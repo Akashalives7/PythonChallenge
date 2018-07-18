6007bcd8eeb79d5fb6067054311133322a324d8f
Akashalives7 <eleanorac@hotmail.com>
2018-07-18 11:20:24 -0700

    GG

--- PyBank/mainBudget.py
+++ PyBank/mainBudget.py
@@ -14,7 +14,7 @@ revenue_total = 0
 csvpath=os.path.join("budget_data.csv")
 
 # reading csv file
-with open(csvpath, 'r') as csvfile:    
+with open(csvpath, 'r') as csvfile:
     # creating a csv reader object
     csvreader = csv.reader(csvfile, delimiter=',')
 
@@ -24,6 +24,7 @@ with open(csvpath, 'r') as csvfile:
     # Count the number of rows without the header to calculate the number of months
     row_count = sum(1 for line in open("budget_data.csv"))
     max_gain = 1
+    prev_month = 1
     for row in csvreader:
         #print(type(row[1]))
         count += 1
@@ -31,19 +32,18 @@ with open(csvpath, 'r') as csvfile:
         #print(revenue)
 
         this_month = int(row[1])
-        revenue_change = int(row[1]) 
-       
+        revenue_change = int(row[1])
         revenue_total += int(row[1])
-        prev_month = int(row[1])
-        
         revenue + (float(row[2])
-        
+        # change = int(max(this_month,prev_month)) - int(max(this_month,prev_month))
+        numList = [ int(this_month) , int(prev_month) ]
+        largerNum=max( this_month , prev_month )
+        small =  max(this_month,prev_month)
+        ) - int(max(this_month,prev_month))
+        print (change = max(this_month,prev_month) - max(this_month,prev_month)        prev_month = int(row[1]))
         if revenue_change > max_gain:
             max_gain = revenue_total
             print ('Now revenue_total is:' + str (revenue_total) + "on" + str((row[1]))
-
-#     # Sum value of column B or 1 for all rows
-
 
 print ("Total Months: " + str(count))
 print ("Total Revenue: " + str(revenue_total))
